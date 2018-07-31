78d2bba38be906f4f348831e4729a37eef50992a
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2018-07-31 22:25:17 +0300

    data managment

---
+++ R/data.R
@@ -0,0 +1,53 @@
+#' Monthly data
+#'
+#'	year and month
+#'  dividend payout ratio (DE)
+#'	long-term yield (LTY)
+#'	dividend yield (DY)
+#'	dividend-price ratio (DP)
+#'	T-bill rate (TBL)
+#'	earnings-price ratio (EP)
+#'	book-to-market value ratio (BM)
+#'	inflation rate (INF)
+#'	default yield spread (DFY)
+#'	net equity expansion (NTIS)
+#'	term spread (TMS)
+#'	S&P 500 value-weighted log excess returns (LOG EXCESS VW)
+#'
+#' @format A data.frame with 1680 observations
+#' \describe{
+#'   \item{sp.500.ratio}{S and P 500 Price-Dividen Ratio}
+#'  }
+#' @source \url{http://www.econ.yale.edu/~shiller/data.htm}
+"datam"
+
+#' Quarterly data
+#'
+#'	year and month
+#'	dividend payout ratio (DE)
+#'	long-term yield (LTY)
+#'	dividend yield (DY)
+#'	dividend-price ratio (DP)
+#'	T-bill rate (TBL)
+#'	earnings-price ratio (EP)
+#'	book-to-market value ratio (BM)
+#'	inflation rate (INF)
+#'	default yield spread (DFY)
+#'	net equity expansion (NTIS)
+#'	term spread (TMS)
+#'	S&P 500 value-weighted log excess returns (LOG EXCESS VW)
+#'
+#' @format A data.frame with 1680 observations
+#' \describe{
+#'   \item{sp.500.ratio}{S and P 500 Price-Dividen Ratio}
+#'  }
+#' @source \url{http://www.econ.yale.edu/~shiller/data.htm}
+"dataq"
+
+#' Critical values for the lower bound c
+#'
+"c_low_crit"
+
+#' Critical values for the upper bound c
+#'
+"c_up_crit"
